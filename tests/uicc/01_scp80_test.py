1598e373ed19b834684258f986f53243ddf2a756
Telman (GitHub) <void.deref0@gmail.com>
2026-03-08 11:38:35 +0300

    refactored SCP80 unit test

---
+++ tests/uicc/01_scp80_test.py
@@ -0,0 +1,19 @@
+
+from fun_gp.utils import hex_to_bytes, bytes_to_hex, lv
+from fun_gp import UICC
+
+known_readers = ['ACS ACR39U ICC Reader 0']
+iccid         = '8970199 0123456789 1'
+tar_value     = '74 61 72'
+
+text     = 'длина этой строки 29 символов'
+test_mds = lv('08' + bytes_to_hex(text.encode('utf-16-be')))
+
+
+def main():
+    uicc = UICC(known_readers, iccid)
+    uicc.terminal_profile()
+    uicc.apdu_scp80(test_mds, spi='1622', kic=0x09, kid=0x09, tar=tar_value)
+
+main()
+
