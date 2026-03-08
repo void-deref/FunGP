85497a5cfb95326dd3ac06604391aea09ee709bf
Telman (GitHub) <void.deref0@gmail.com>
2026-03-08 12:13:59 +0300

    minor changes

--- tests/uicc/01_scp80_test.py
+++ tests/uicc/01_scp80_test.py
@@ -5,10 +5,11 @@ from fun_gp import UICC
 known_readers = ['ACS ACR39U ICC Reader 0']
 iccid         = '8970199 0123456789 1'
 tar_value     = '74 61 72'
+card_deck_path = '../../resources/known-simcards.json'
 
 text     = 'длина этой строки 29 символов'
 test_msg = lv('08' + bytes_to_hex(text.encode('utf-16-be')))
-card_deck_path = '../../resources/known-simcards.json'
+
 
 def main():
     uicc = UICC(known_readers, iccid, card_deck_path)
