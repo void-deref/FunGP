8398dea418179d78868b1bc1f18a8de6e4360b9f
Telman (GitHub) <void.deref0@gmail.com>
2026-06-22 12:11:59 +0300

    bugfix: passing over correct install parameters

--- fun_gp/__init__.py
+++ fun_gp/__init__.py
@@ -4,6 +4,11 @@ from fun_gp.scp02 import SCP02
 from fun_gp.ccm import CardContentManagement, ForLoad, LoadParams, ForInstall, InstallParams
 from fun_gp.smart_card import SmartCard
 
+from pathlib import Path
+
+BASE_DIR    = Path(__file__).resolve().parent
+KEYS_PATH   = BASE_DIR / ".." / "resources"
+APPLET_PATH = BASE_DIR / ".." / "resources"
 
 __all__ = [
     "SW_list", "Reader", "Card", "CardDeck",
--- tests/smart_card/05_install_for_install_test.py
+++ tests/smart_card/05_install_for_install_test.py
@@ -1,18 +1,20 @@
-from fun_gp import SmartCard
-from fun_gp.utils import lv_hex
+from fun_gp import SmartCard, InstallParams, APPLET_PATH
+from fun_gp.utils import lv_hex, hex_to_bytes
 
-known_readers    = ['ACS ACR39U ICC Reader 0', 'ACS ACR39U ICC Reader 00 00']
-isd_default_keys = ["404142434445464748494A4B4C4D4E4F","404142434445464748494A4B4C4D4E4F","404142434445464748494A4B4C4D4E4F"]
-package_aid      = 'A000000082', # 'SimpleApplet.cap'
-applets = [
-    "../../resources/SimpleApplet.cap", # 'A000000082'
-]
-app_params = '00112233445566778899'
+known_readers = ['ACS ACR39U ICC Reader 0', 'ACS ACR39U ICC Reader 00 00']
+isd_default_keys = ["404142434445464748494A4B4C4D4E4F",
+                    "404142434445464748494A4B4C4D4E4F",
+                    "404142434445464748494A4B4C4D4E4F"]
+
+applet = APPLET_PATH / 'SimpleApplet.cap' # 'A000000082'
+
+
+install_params = InstallParams(
+    app_params=lv_hex('010203'),
+)
 
 isd = SmartCard(known_readers, isd_default_keys)
 
 isd.apdu_plain('00A4 0400' + lv_hex('a000000151000000'), expected_sw=0x9000, name='SELECT: isd')
 isd.mutual_auth()
-
-for cap in applets:
-    isd.install_app_scp02(cap, app_params)
+isd.install_app_scp02(applet, install_params)
