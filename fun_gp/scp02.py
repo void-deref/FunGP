f6ddac39c5c76f5fe801c01279f368d735f69b7f
Telman (GitHub) <void.deref0@gmail.com>
2026-07-18 20:24:04 +0300

    refacoted scp02.py module

--- fun_gp/scp02.py
+++ fun_gp/scp02.py
@@ -2,9 +2,9 @@ from fun_gp.utils import bytes_to_hex, hex_to_bytes
 from Crypto.Cipher import DES3, DES
 
 
-# class SCP02(Reader):
 class SCP02:
-    def __init__(self, key_set:list=['','','']):
+    def __init__(self, key_set:list[str, str, str]):
+        
         self.enc = key_set[0]
         self.mac = key_set[1]
         self.dec = key_set[2]
@@ -16,7 +16,22 @@ class SCP02:
         
         self.authenticated = False
 
-    def _init_update(self, response, host_challenge):
+
+    def make_scp02_packet(self, cmd:str|list) ->list[int]:
+
+        if isinstance(cmd, str):
+            cmd = hex_to_bytes(cmd)
+        
+        cmd[0] &= 0xFC # clean channel indication
+        cmd[0] |= 0x04 # set GP proprietary SM flag
+        cmd[4] += 8
+        if cmd[4] > 255:
+            raise ValueError(f'Expected 255 bytes for CDATA, but got {cmd[4]}')
+        cmd += self._retail_mac(cmd)
+        return cmd
+
+    
+    def init_update(self, response, host_challenge):
         counter,              \
         card_challenge,       \
         card_cryptogram,      \
@@ -37,15 +52,16 @@ class SCP02:
         
         if card_cryptogram != card_cryptogram_check:
             raise ValueError(
-                f'ERROR: cryptograms mismatch!\
-                \n\t\t\t****************************** CAUTION! ******************************\
+                '\t\t\tERROR: cryptograms mismatch!\
+                \n\t\t\t****************************** NOTICE! ******************************\
                 \n\t\t\tYou see this message because \'INITIALIZE UPDATE\' command failed.\
                 \n\t\t\tAfter 5 or more attemts ISD can intentionally increase the time of \
                 \n\t\t\tperformig this operation because he thinks you\'re brute-forcing him.')
        
         return counter, card_challenge, host_challenge
 
-    def _external_authenticate(self, counter, card_challenge, host_challenge):
+
+    def external_authenticate(self, counter, card_challenge, host_challenge):
         # GP, appendix E.4.2.2: host authentication cryptogram
         host_crypto  = self._host_crypto(counter, card_challenge, host_challenge)
         ext_auth_cmd = [0x80, 0x82, 0x01, 0x00, 0x08] + host_crypto
@@ -57,6 +73,7 @@ class SCP02:
         ext_auth_cmd = ext_auth_cmd + c_mac
         return ext_auth_cmd
         
+
     def _host_crypto(self, counter, card_challenge, host_challenge):
         auth_data = counter + card_challenge + host_challenge + [0x80] + [0] * 7
         auth_data = self._apply_3des_cbc(auth_data, self.session_enc)
@@ -65,6 +82,7 @@ class SCP02:
         print(f'\t\thost cryptogram         : {bytes_to_hex(auth_data)}\n')
         
         return auth_data
+
 
     def _card_crypto(self, host_challenge, counter, card_challenge):
         auth_data = host_challenge + counter + card_challenge + [0x80] + [0] * 7
@@ -75,9 +93,10 @@ class SCP02:
         
         return auth_data
     
+
     def _derive_key(self, key, sequence_counter, key_type):
-        plain_text = [1, 0] # the first byte of constant value. See GP 2.3, appendix E.4
-        # define the second byte of constant value
+        plain_text = [1, 0] # the first byte is always '1'. See GP 2.3, appendix E.4
+        # define the second byte
         if (key_type == 'mac'):
             plain_text[1] = 0x01
         elif (key_type == 'enc'):
@@ -95,6 +114,7 @@ class SCP02:
         # print(f'\t\t{key_type}                     : {bytes_to_hex(session_key)}')
         return session_key
 
+
     def _apply_3des_cbc(self, plain_text, key):
         if isinstance(key, str):
             key = bytes(hex_to_bytes(key))
@@ -106,6 +126,7 @@ class SCP02:
         cipher = DES3.new(key, DES3.MODE_CBC, (b'\x00' * 8))
         return cipher.encrypt(bytes(plain_text))
 
+
     def _parse_card_response(self, response):
         diversification_data = response[0:10]
         kvn_and_scp_id       = response[10:12]
@@ -115,34 +136,36 @@ class SCP02:
         
         return counter, card_challenge, card_cryptogram, diversification_data, kvn_and_scp_id
 
-    def _retail_mac(self, cmd:list):
+
+    def _retail_mac(self, input_list:list):
         
         # According to ISO 9797-1
         # Step 1: padding method 2
-        cmd = cmd + [0x80]
-        data_len = len(cmd)
+        payload  = list(input_list)
+        payload += [0x80]
+        data_len = len(payload)
         padding  = (8 - (data_len % 8)) % 8
-        cmd     += [0] * padding
+        payload += [0] * padding
 
         # cmd[4] += 8
-        des_ecb =  DES.new(self.session_mac[0:8], DES.MODE_ECB)
+        des_ecb  = DES.new(self.session_mac[0:8], DES.MODE_ECB)
         des3_ecb = DES3.new(self.session_mac, DES3.MODE_ECB)
 
         if self.authenticated == True:
             self.IV = list(des_ecb.encrypt(bytes(self.IV)))
-        
+
         i = 0
         # Steps 2-4: splitting, initial trasformation and iteration
-        while i < (len(cmd) - 8):
+        while i < (len(payload) - 8):
             for j in range(8):
-                self.IV[j] ^=cmd[i + j]
+                self.IV[j] ^= payload[i + j]
             
             self.IV = list(des_ecb.encrypt(bytes(self.IV)))
             i += 8
         
         # step 5: output transformation
         for j in range(8):
-            self.IV[j] ^=cmd[i + j]
+            self.IV[j] ^= payload[i + j]
         
         self.IV = list(des3_ecb.encrypt(bytes(self.IV)))   
         return self.IV
