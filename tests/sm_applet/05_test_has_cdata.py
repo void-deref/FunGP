0a1565dfe57db7bd2f736d4fa39ffdd43a1e9c80
Telman Islamov <void.deref0@gmail.com>
2026-07-22 17:53:16 +0300

    refactored 05_test_has_cdata.py script

--- tests/sm_applet/05_test_has_cdata.py
+++ tests/sm_applet/05_test_has_cdata.py
@@ -7,8 +7,7 @@ def main():
     with Reader() as reader:
         isd = SmartCard(reader.plain_apdu)
         isd.transmit('00a4 0400' + lv_hex('A0000000837365636D7367'), 0x90, 0x00, cmd_name='Select Secure Message applet')
-        card_pub_key,_,_ = isd.transmit('8002 0000' + lv_hex(dh.pub_key), 0x90, 0x00, cmd_name='Public key exchange between Host and Card')
-        
-        shared_secret = dh.generate_shared_secret(card_pub_key)
 
-        dh.init_aes_cipher(shared_secret[0:16])
+        card_pub_key,_,_ = isd.transmit('8002 0000', 0x67, 0x00, cmd_name='Public key exchange between Host and Card')
+
+main()
